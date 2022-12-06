38255779bb2558c6973dbb2a0a1c1a84090918b2
djolekrsticc <jddkrstic@gmail.com>
2022-12-06 05:19:58 +0100

    Services updated

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,5 +1,4 @@
-import { useSpring, animated } from "@react-spring/web";
-import {useEffect, useRef, useState} from "react";
+import {useEffect, useState} from "react";
 import {HiArrowLeft} from "react-icons/hi2";
 import {HiArrowRight} from "react-icons/hi2";
 import services from "../json/services.json";
@@ -9,8 +8,8 @@ const Services = () => {
 
     useEffect(() => {
         const element = document.getElementById(id);
-        element.scrollIntoView({ behavior: "smooth", inline: "center" });
-      }, [id]);
+        element.scrollIntoView({behavior: "smooth", inline: "center"});
+    }, [id]);
 
     return (
         <section id="services">
@@ -28,72 +27,110 @@ const Services = () => {
                     </div>
 
                     <div className="hidden md:flex flex-row items-center justify-evenly w-2/12">
-                        <div onClick={() => {
-                            setId(id-1);
-                            
-                        }}
+                        <div onClick={
+                                () => {
+                                    if (id != 0) {
+                                        setId(id - 1);
+                                    } else {
+                                        id = id
+                                    }
+                                }
+                            }
                             className="services-arrow">
                             <HiArrowLeft className="w-12 h-12 text-white"/>
                         </div>
 
-                        <div onClick={() => {
-                            setId(id+1);
-                            
-                        }}
+                        <div onClick={
+                                () => {
+                                    if (id != services.length - 1) {
+                                        setId(id + 1);
+                                    } else {
+                                        id = id
+                                    }
+                                }
+                            }
                             className="services-arrow">
                             <HiArrowRight className="w-12 h-12 text-white"/>
                         </div>
                     </div>
                 </div>
 
-            <div className="flex flex-col justify-center items-start overflow-y-visible overflow-x-hidden">
-                <div className="py-12 md:py-20 mx-7 md:mx-0">
-                    <div className="flex flex-row items-center justify-between gap-x-12 md:gap-x-5">
-                        {
-                        services.map(service => <a key={
-                                service.id
-                            }
-                            href={
-                                service.id
-                        }
-                            id={service.id}
-                        >
-                            <div className="w-80 h-96 md:w-112 md:h-128 bg-white border border-slate-500 rounded-3xl group">
-                                <div>
-                                    <img className="hidden md:flex md:group-hover:hidden" style={{ width: '95px' }} src={service.png} alt={service.name} /> 
-                                    <img className="md:hidden md:group-hover:flex my-7 mx-7" style={{ width: '130px' }} src={service.gif} alt={service.name} /> 
+                <div className="flex flex-col justify-center items-start overflow-y-visible overflow-x-hidden">
+                    <div className="py-12 md:py-20 mx-7 md:mx-0">
+                        <div className="flex flex-row items-center justify-between gap-x-12 md:gap-x-5">
+                            {
+                            services.map(service => <a key={
+                                    service.id
+                                }
+                                href={
+                                    service.id
+                                }
+                                id={
+                                    service.id
+                            }>
+                                <div className="w-80 h-96 md:w-112 md:h-128 bg-white border border-slate-500 rounded-3xl group">
+                                    <div>
+                                        <img className="hidden md:flex md:group-hover:hidden"
+                                            style={
+                                                {width: '95px'}
+                                            }
+                                            src={
+                                                service.png
+                                            }
+                                            alt={
+                                                service.name
+                                            }/>
+                                        <img className="md:hidden md:group-hover:flex my-7 mx-7"
+                                            style={
+                                                {width: '130px'}
+                                            }
+                                            src={
+                                                service.gif
+                                            }
+                                            alt={
+                                                service.name
+                                            }/>
+                                    </div>
+
+                                    <div></div>
+
+                                    <div></div>
+
+                                    <div></div>
                                 </div>
+                            </a>)
+                        } </div>
+                    </div>
 
-                                <div></div>
-
-                                <div></div>
-
-                                <div></div>
-                            </div>
-                        </a>)
-                    } </div>
                 </div>
+                <div className="flex md:hidden flex-row items-center justify-center w-full">
+                    <div onClick={
+                            () => {
+                                if (id != 0) {
+                                    setId(id - 1);
+                                } else {
+                                    id = id
+                                }
+                            }
+                        }
+                        className="services-arrow-small">
+                        <HiArrowLeft className="w-8 h-8 text-white"/>
+                    </div>
 
-            </div>
-            <div className="flex md:hidden flex-row items-center justify-center w-full">
-                        <div onClick={() => {
-                            setId(id-1);
-                            
-                        }}
-                            className="services-arrow-small">
-                            <HiArrowLeft className="w-8 h-8 text-white"/>
-                        </div>
-
-                        <div onClick={() => {
-                            setId(id+1);
-                            
-                            
-                        }}
-                            className="services-arrow-small">
-                            <HiArrowRight className="w-8 h-8 text-white"/>
-                        </div>
+                    <div onClick={
+                            () => {
+                                if (id != services.length - 1) {
+                                    setId(id + 1);
+                                } else {
+                                    id = id
+                                }
+                            }
+                        }
+                        className="services-arrow-small">
+                        <HiArrowRight className="w-8 h-8 text-white"/>
                     </div>
                 </div>
+            </div>
         </section>
     )
 }
